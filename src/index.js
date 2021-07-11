6c8c1aad54f270ab3faa3b7622a0666f62452722
Abraham Azael <aamr117cazul@gmail.com>
2021-07-11 14:53:14 -0500

    Custom Hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
+import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './Components/01-useState/CounterApp';
 // import { CounterWithCustomHooks } from './Components/01-useState/CounterWithCustomHooks';
-import { SimpleForm } from './Components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './Components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
-  <SimpleForm />,
+  <FormWithCustomHook />,
   document.getElementById('root')
 );
 
