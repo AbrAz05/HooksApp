ac43ad027e41f9eaf25daef9582b1ddbeae61917
Abraham Azael <aamr117cazul@gmail.com>
2021-07-11 01:20:58 -0500

    UseState y UseEffect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+// import { CounterApp } from './Components/01-useState/CounterApp';
+// import { CounterWithCustomHooks } from './Components/01-useState/CounterWithCustomHooks';
+import { SimpleForm } from './Components/02-useEffect/SimpleForm';
+// import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
-  <HookApp />,
+  <SimpleForm />,
   document.getElementById('root')
 );
 
