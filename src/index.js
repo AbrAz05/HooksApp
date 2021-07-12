a5ec50fb33b154583278379c6e5fc19c79f43d16
Abraham Azael <aamr117cazul@gmail.com>
2021-07-11 19:13:18 -0500

    UseRef

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './Components/04-useRef/FocusScreen';
+import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
 // import { CounterApp } from './Components/01-useState/CounterApp';
 // import { CounterWithCustomHooks } from './Components/01-useState/CounterWithCustomHooks';
 // import { SimpleForm } from './Components/02-useEffect/SimpleForm';
@@ -7,7 +10,7 @@ import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook
 
 
 ReactDOM.render(
-  <FormWithCustomHook />,
+  <RealExampleRef />,
   document.getElementById('root')
 );
 
