b8ec6b3aee1f3ef0169865a2001b5425db462d9c
Abraham Azael <aamr117cazul@gmail.com>
2021-07-13 22:36:48 -0500

    Hooks App Completa

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom';
+// import { CallbackHook } from './Components/06-memos/CallbackHook';
+import { Padre } from './Components/07-tarea-memo/Padre';
+// import { MemoHook } from './Components/06-memos/MemoHook';
 // import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './Components/04-useRef/FocusScreen';
-import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
+// import { Layout } from './Components/05-useLayoutEffect/Layout';
+// import { Memorize } from './Components/06-memos/Memorize';
 // import { CounterApp } from './Components/01-useState/CounterApp';
 // import { CounterWithCustomHooks } from './Components/01-useState/CounterWithCustomHooks';
 // import { SimpleForm } from './Components/02-useEffect/SimpleForm';
@@ -10,7 +15,7 @@ import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
 
 
 ReactDOM.render(
-  <RealExampleRef />,
+  <Padre />,
   document.getElementById('root')
 );
 
